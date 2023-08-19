71ca10685614df99a37a48518ae346058ebb9b68
Ali-Adel-Nour <aliiiadel841@gmail.com>
2023-08-19 13:38:38 +0300

    Best to buy and sell

---
+++ problems/Array/121. Best Time to Buy and Sell Stock/app.js
@@ -0,0 +1,16 @@
+const maxProfit = (prices) => {
+  let left = 0; // Buy
+  let right = 1; // sell
+  let max_profit = 0;
+  while (right < prices.length) {
+    if (prices[left] < prices[right]) {
+      let profit = prices[right] - prices[left]; // our current profit
+
+      max_profit = Math.max(max_profit, profit);
+    } else {
+      left = right;
+    }
+    right++;
+  }
+  return max_profit;
+};
