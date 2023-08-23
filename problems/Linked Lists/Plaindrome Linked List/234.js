e2e8a491b99b269be04acb7d9ae9415e63905ce1
Ali-Adel-Nour <aliiiadel841@gmail.com>
2023-08-23 11:02:14 +0300

    Find Plaindrom in Single Linked List

---
+++ problems/Linked Lists/Plaindrome Linked List/234.js
@@ -0,0 +1,14 @@
+var isPalindrome = function(head) {
+
+  let slow = head, fast = head, prev, temp
+   while (fast && fast.next)
+       slow = slow.next, fast = fast.next.next
+   prev = slow, slow = slow.next, prev.next = null
+   while (slow)
+       temp = slow.next, slow.next = prev, prev = slow, slow = temp
+   fast = head, slow = prev
+   while (slow)
+       if (fast.val !== slow.val) return false
+       else fast = fast.next, slow = slow.next
+   return true
+}
