ef3e077e2623b424871f1af3bf4fee0d5e703ad4
Ali-Adel-Nour <aliiiadel841@gmail.com>
2023-09-05 13:42:03 +0300

    using oop

--- Stack/oopS.js
+++ Stack/oopS.js
@@ -1,5 +1,56 @@
 class Stack {
   constructor(){
-
+    this.items = []
   }
+
+  push(element){
+  this.items.push(element)
+  }
+
+  pop()
+  {
+
+      if (this.items.length == 0)
+          return "Underflow";
+      return this.items.pop();
+  }
+  peek()
+{
+
+    return this.items[this.items.length - 1];
 }
+isEmpty()
+{
+
+    return this.items.length == 0;
+}
+printStack()
+{
+    let str = "";
+    for (let i = 0; i < this.items.length; i++)
+        str += this.items[i] + " ";
+    return str;
+}
+}
+let stack = new Stack();
+
+
+console.log(stack.isEmpty());
+
+
+
+stack.push(10);
+stack.push(20);
+stack.push(30);
+
+console.log(stack.printStack());
+
+console.log(stack.pop());
+
+console.log(stack.peek());
+
+
+console.log(stack.pop());
+
+
+console.log(stack.printStack());
