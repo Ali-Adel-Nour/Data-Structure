bb332d80d72cf37cb618ff44d4a56b67c0755f97
Ali-Adel-Nour <aliiiadel841@gmail.com>
2023-10-12 11:31:52 +0300

    Armstrong Number

---
+++ problems/Array/Armstrong Number/app.js
@@ -0,0 +1,8 @@
+export const isArmstrongNumber = (input) => {
+  const digits = [...String(input)];
+  const sum = digits.reduce(
+    (total, current) => total + current ** digits.length,
+    0,
+  );
+  return sum === input;
+}
