15696627cb5af0917ac25a9ea5f60510238479bf
Ali-Adel-Nour <aliiiadel841@gmail.com>
2023-11-15 19:33:04 +0200

    BFS

---
+++ Binary Tree/BFS.js
@@ -0,0 +1,17 @@
+const breadthFirstValues = (root) => {
+  if (root === null) return [];
+
+  const values = [];
+  const queue = [root];
+
+  while (queue.length > 0) {
+    const node = queue.shift();
+    values.push(node.val);
+
+    if (node.left !== null) queue.push(node.left);
+
+    if (node.right !== null) queue.push(node.right);
+  }
+
+  return values;
+};
