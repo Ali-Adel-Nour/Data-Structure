10631fcf26d14483f7be1bdeeed284ec82516723
Ali-Adel-Nour <aliiiadel841@gmail.com>
2024-01-10 21:25:37 +0200

    Merge string alt

---
+++ problems/Two Pointers/app.js
@@ -0,0 +1,10 @@
+var mergeAlternately = function (word1, word2) {
+  const buffer = [];
+
+  for (let i = 0; i < word1.length || i < word2.length; i++) {
+      if (i < word1.length) buffer.push(word1[i]);
+      if (i < word2.length) buffer.push(word2[i]);
+  }
+
+  return buffer.join('');
+};
