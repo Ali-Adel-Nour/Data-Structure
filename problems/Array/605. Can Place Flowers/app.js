7069459c8589985a32ab458d0079da1d1ea918bc
Ali-Adel-Nour <aliiiadel841@gmail.com>
2024-01-13 20:56:27 +0200

    can place flowers

---
+++ problems/Array/605. Can Place Flowers/app.js
@@ -0,0 +1,13 @@
+var canPlaceFlowers = function(flowerbed, n) {
+  for (let i = 0; i < flowerbed.length && n !== 0; i++) {
+  if (
+    flowerbed[i] === 0 &&
+    flowerbed[i - 1] !== 1 &&
+    flowerbed[i + 1] !== 1
+  ) {
+    n--;
+    i++;
+  }
+}
+return n === 0;
+}
