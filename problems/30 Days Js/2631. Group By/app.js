23a48c4fa820263dfeb5eab95648372f34aa7d23
Ali-Adel-Nour <aliiiadel841@gmail.com>
2024-02-07 11:33:23 +0200

    simpler code for Group By

--- problems/30 Days Js/2631. Group By/app.js
+++ problems/30 Days Js/2631. Group By/app.js
@@ -1,3 +1,4 @@
+/*
 Array.prototype.groupBy = function(fn){
   const res = {}
 
@@ -10,3 +11,23 @@ Array.prototype.groupBy = function(fn){
 
 
 }
+*/
+
+Array.prototype.groupBy = function(fn) {
+  // Reduce the array into a single object
+  return this.reduce((grouped, item) => {
+    // Apply the provided callback function to get the key
+    const key = fn(item);
+
+    // If the key doesn't exist in the grouped object, create a new array for it
+    if (!grouped[key]) {
+      grouped[key] = [];
+    }
+
+    // Push the current item to the array associated with the key
+    grouped[key].push(item);
+
+    // Return the updated grouped object for the next iteration
+    return grouped;
+  }, {});
+};
