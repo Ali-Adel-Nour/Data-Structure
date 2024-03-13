78e8ecb7a102f5a2faf9c930f35857bcfa4e1cbd
Ali-Adel-Nour <aliiiadel841@gmail.com>
2024-03-13 14:26:57 +0200

    adding other code

--- Algorithms/Sorting/InsertionSort.js
+++ Algorithms/Sorting/InsertionSort.js
@@ -12,4 +12,22 @@ function sortList(unsortedList) {
   return unsortedList;
 }
 
+
+
+//another code
+function insertion(A, n) {
+  let i, j, x;
+  for (i = 1; i < n; i++) {
+    j = i - 1;
+    x = A[i];
+    while (j > -1 && A[j] > x) {
+      A[j + 1] = A[j];
+      j--;
+    }
+    A[j + 1] = x;
+  }
+}
+
+
+
 console.log(sortList([10, 1, 20]));
