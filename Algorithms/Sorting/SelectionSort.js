b4ee36b9184c41d94afd9390bb30c3347278e1fc
Ali-Adel-Nour <aliiiadel841@gmail.com>
2024-03-20 13:02:09 +0200

    trying to make selection sort stable

--- Algorithms/Sorting/SelectionSort.js
+++ Algorithms/Sorting/SelectionSort.js
@@ -16,3 +16,35 @@ function sortList(unsortedList) {
   }
   return unsortedList;
 }
+
+
+
+//Putting numer on right place to make it stable
+
+function stableSelectionSort(a, n)
+{
+    // Iterate through array elements
+    for (let i = 0; i < n - 1; i++)
+    {
+
+        // Loop invariant : Elements till
+        // a[i - 1] are already sorted.
+
+        // Find minimum element from
+        // arr[i] to arr[n - 1].
+        let min = i;
+        for (let j = i + 1; j < n; j++)
+            if (a[min] > a[j])
+                min = j;
+
+        // Move minimum element at current i.
+        let key = a[min];
+        while (min > i)
+        {
+            a[min] = a[min - 1];
+            min--;
+        }
+
+        a[i] = key;
+    }
+}
