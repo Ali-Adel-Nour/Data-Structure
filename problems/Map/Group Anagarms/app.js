764e19465e4cb4329368db2445935282f3d4366d
Ali-Adel-Nour <aliiiadel841@gmail.com>
2024-08-07 21:10:18 +0300

    easier soultion

--- problems/Map/Group Anagarms/app.js
+++ problems/Map/Group Anagarms/app.js
@@ -17,3 +17,18 @@ var groupAnagrams = function(strs) {
 
   return ans;
 };
+
+
+
+
+
+var groupAnagrams = function(strs) {
+  let map = {}
+
+  for(let str of strs){
+      let s = str.split('').sort().join('')
+      if(!map[s]) map[s] = []
+      map[s].push(str)
+  }
+  return Object.values(map)
+};
