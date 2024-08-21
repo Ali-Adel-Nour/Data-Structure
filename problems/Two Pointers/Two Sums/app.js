7011f391746584d5719814d277067ac1b54d7106
Ali-Adel-Nour <aliiiadel841@gmail.com>
2024-08-21 15:19:41 +0300

    Two Sums

---
+++ problems/Two Pointers/Two Sums/app.js
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/two-sum/
+
+var twoSum = function(array, targetValue) {
+  let l = 0;
+ let r = arr.length - 1;
+ while (l < r) {
+     const two_sum = arr[l] + arr[r];
+     if (two_sum === target) {
+         return [l, r];
+     }
+     if (two_sum < target) {
+         l++;
+     } else {
+         r--;
+     }
+ }
