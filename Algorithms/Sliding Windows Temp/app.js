2dd424b3f6febbd25a3d65eaa0c7fdf1496d5cef
Ali-Adel-Nour <aliiiadel841@gmail.com>
2024-08-22 13:57:27 +0300

    Siding windows temp

--- Algorithms/Sliding Windows Temp/app.js
+++ Algorithms/Sliding Windows Temp/app.js
@@ -1,9 +1,9 @@
 function slidingWindowFixed(input, windowSize) {
-  ans = window = input[0..windowSize)
+  //ans = window = input[0..windowSize)
   for (const right = windowSize; right < input.length; ++right) {
       const left = right - windowSize;
-      remove input[left] from window
-      append input[right] to window
+      //remove input[left] from window
+      //append input[right] to window
       ans = optimal(ans, window);
   }
   return ans
