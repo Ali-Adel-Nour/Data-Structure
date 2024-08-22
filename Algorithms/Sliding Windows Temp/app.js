cd0a81c66ec74eb46d3189f91670724653f30aa9
Ali-Adel-Nour <aliiiadel841@gmail.com>
2024-08-22 13:49:14 +0300

    sliding windows template

---
+++ Algorithms/Sliding Windows Temp/app.js
@@ -0,0 +1,10 @@
+function slidingWindowFixed(input, windowSize) {
+  ans = window = input[0..windowSize)
+  for (const right = windowSize; right < input.length; ++right) {
+      const left = right - windowSize;
+      remove input[left] from window
+      append input[right] to window
+      ans = optimal(ans, window);
+  }
+  return ans
+}
