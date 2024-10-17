5e113e989b44dad883da097b7317f717ee3beedf
Ali-Adel-Nour <aliiiadel841@gmail.com>
2024-10-17 12:30:23 +0300

    Same Tree

---
+++ problems/Binary Search Tree/Same Tree/app.js
@@ -0,0 +1,22 @@
+
+
+
+var isSameTree = function(p, q) {
+
+  if (!p && !q) {
+      return true;
+  }
+
+
+  if (!p || !q) {
+      return false;
+  }
+
+
+  if (p.val !== q.val) {
+      return false;
+  }
+
+
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
