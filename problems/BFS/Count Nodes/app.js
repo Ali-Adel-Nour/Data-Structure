3c98e5358bda4ca650b314692b56790eae254d8a
Ali-Adel-Nour <aliiiadel841@gmail.com>
2024-11-17 12:08:20 +0200

    count nodes

---
+++ problems/BFS/Count Nodes/app.js
@@ -0,0 +1,23 @@
+var countNodes = function(root) {
+  if(root == null){
+      return 0
+  }
+
+const queue = [root]
+let count = 0
+
+while(queue.length>0){
+const levelSize = queue.length
+let levelResult = []
+
+for(let i =0 ; i<levelSize ; i++){
+  const node = queue.shift()
+  levelResult.push(node.val)
+  count ++
+  if (node.left) queue.push(node.left);
+        if (node.right) queue.push(node.right);
+    }
+
+}
+return count
+};
