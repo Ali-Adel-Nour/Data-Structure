4583ed2a5bfc3d572c7b3191dec0b715ca6b13d6
Ali-Adel-Nour <aliiiadel841@gmail.com>
2024-12-17 10:39:29 +0200

    valid plaindrom II

---
+++ problems/Two Pointers/Valid Plaindrome II/app.js
@@ -0,0 +1,19 @@
+var validPalindrome = function(s) {
+  let low = 0, high = s.length-1;
+  while (low < high) {
+      if (s[low] !== s[high]) {
+          return isPalindrome(s, low+1, high) || isPalindrome(s, low, high-1);
+      }
+      low++, high--;
+  }
+  return true;
+
+};
+
+function isPalindrome(str, low, high) {
+  while (low < high) {
+      if (str[low] !== str[high]) return false;
+      low++, high--;
+  }
+  return true;
+}
