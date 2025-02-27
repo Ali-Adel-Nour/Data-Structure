4be27002ccc04ef13eff634ff3afd985af79adc1
Ali-Adel-Nour <aliiiadel841@gmail.com>
2025-02-27 11:53:40 +0200

    Remove Element

---
+++ problems/Array/Remove Element/app.js
@@ -0,0 +1,10 @@
+var removeElement = function(nums, val) {
+  let k = 0;
+  for(let i = 0; i < nums.length; i++) {
+      if(nums[i] !== val) {
+          nums[k] = nums[i];
+          k++;
+      }
+  }
+  return k;
+};
